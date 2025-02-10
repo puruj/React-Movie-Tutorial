ee509e960efb50f5971fcedafeaa4f577050ede3
puruj <puru123456789@hotmail.com>
2025-02-10 12:31:22 -0500

    Gotten initial movie card displaying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
   return (
-  <div></div>
+  <div>
+    <MovieCard movie={{title: 'The Godfather', release_date: 'March 24, 1972', url: 'https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg'}} />
+    <MovieCard movie={{title: 'The Shawshank Redemption', release_date: 'September 23, 1994', url: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg'}} />
+  </div>
   )
 }
 
