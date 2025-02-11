7c8d0d5fc2e8b5fdbdd99e3aeb0eb56d40ae4fc9
puruj <puru123456789@hotmail.com>
2025-02-10 22:58:13 -0500

    favorites is functional now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import {Routes , Route } from 'react-router-dom'
 import './css/App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
+import { MovieProvider } from './contexts/MovieContext'
 import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
